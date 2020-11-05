88a9407000e5599469e5bad9eb1c31ba69b0bc66
MissPigHead <misspighead@gmail.com>
2020-11-05 16:18:38 +0800

    layout with Bootstrap, need to fix the var issue, for next/last month

--- calendar.php
+++ calendar.php
@@ -3,139 +3,148 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calendar_1029Homework</title>
+    <title>Calendar</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
-    <style>
-        table{
-            width: 100%;
-            table-layout: fixed;
-        }
-    </style>
 </head>
 <body>
-    <?php
-        $testDate="2020-05-20"; // 輸入測試日期
-        $testDateYrMn=date("Y-m",strtotime($testDate)); //回傳測試日期該年份及月份
-        $firstDateWeek=date("w",(strtotime($testDateYrMn.'-1'))); // 回傳測試該月第一日星期幾
-        $testMonthDays=date("t",strtotime($testDate)); // 回傳測試該月共幾天
-        $endDateWeek=date("w",(strtotime($testDateYrMn.'-'.$testMonthDays))); // 回傳測試該月最後一日星期幾
-
-        $lastMnLastDate=date("Y-m-d",((strtotime($testDateYrMn.'-1'))-24*60*60)); // 回傳前月最後一天日期
-        $lastMnYrMn=date("Y-m",strtotime($lastMnLastDate)); //回傳前月年份及月份
-        $lastMnDays=date("t",strtotime($lastMnLastDate)); //回傳前月共幾天
-        $lastMnFirstDtWk=date("w",(strtotime($lastMnYrMn.'-1'))); //回傳前月第一天星期幾
-        $lastMnLastDtWk=date("w",(strtotime($lastMnLastDate))); //回傳前月最後一天星期幾
-
-        $nextMnFirstDate=date("Y-m-d",((strtotime($testDateYrMn.'-'.$testMonthDays))+24*60*60)); // 回傳次月第一天日期
-        $nextMnYrMn=date("Y-m",strtotime($nextMnFirstDate)); //回傳次月年份及月份
-        $nextMnDays=date("t",strtotime($nextMnFirstDate)); //回傳次月共幾天
-        $nextMnFirstDtWk=date("w",(strtotime($nextMnFirstDate))); //回傳次月第一天星期幾
-        $nextMnLastDtWk=date("w",(strtotime($nextMnYrMn.'-'.$nextMnDays))); //回傳次月最後一天星期幾
-    ?>
-    <h1 class="text-center p-3">Calendar</h1>
     <div class="container">
-        <div class="row justify-content-center text-center">
-            <div class="col-md-10">
-                <h3 class="text-center"><?php echo $testDateYrMn ?></h3>
-                <table class="table table-bordered">
-                    <thead class="bg-dark text-light">
-                        <th>Sun</th>
-                        <th>Mon</th>
-                        <th>Tue</th>
-                        <th>Wed</th>
-                        <th>Thur</th>
-                        <th>Fri</th>
-                        <th>Sat</th>
-                    </thead>
-                    <tbody class="bg-light">
-                    <?php
-                        for ($i=0; $i<=ceil(($testMonthDays+$firstDateWeek-7)/7); $i++){
-                            echo "<tr>";
-                            for ($j=0; $j<=6; $j++){
-                                if ($i==0 && $j<$firstDateWeek){
-                                    echo "<td> &nbsp; </td>";
-                                }else if($i==ceil(($testMonthDays+$firstDateWeek-7)/7) && $j>$endDateWeek){
-                                    echo "<td> &nbsp; </td>";
-                                }else{
-                                    echo "<td>";
-                                    echo ($i*7)+($j+1)-$firstDateWeek;
-                                    echo "</td>";
-                                }
-                            }
-                            echo "</tr>";
-                        }
-                    ?>
-                    </tbody>
-                </table>
+        <div class="row pt-4 d-flex justify-content-center">
+            <div class="mx-1 col-12 col-lg-7 order-2 order-lg-1 card bg-dark text-light text-center">   
+                    <div class="card-body d-flex flex-column">
+                        <div class="order-2">
+                            <form action="calendar_layout.php" method="get" name="calendar">
+                                <div class="form-row d-flex justify-content-center">
+                                    <div class="form-group col-md-2 mr-2 d-none d-md-block">
+                                        <input type="submit" class="btn btn-info col-12" style="font-size: 0.7rem;padding:0.525rem 0.1rem;" name="lastMn" value="Last Month">
+                                            <?php
+                                                date_default_timezone_set('Asia/Taipei');//預設時區
+                                                $timezone = date_default_timezone_get();//叫出時區
+                                                $calendarYrMn=date('Y-F',strtotime(date('Y-m-d')));
+                                                $calendarDt=date('Y-m-d');
+                                                $calendarYr=date('Y',strtotime($calendarDt));//查詢年份，預設now
+                                                $calendarMn=date('m',strtotime($calendarDt));//查詢月份，預設now
+                                                $calendarDy=date('d',strtotime($calendarDt));//查詢riqi，預設now                                                  
+                                                if(isset($_GET['lastMn'])){
+                                                    if ($calendarMn =='01'){
+                                                        $_GET['Yr']=($calendarYr-1);
+                                                        $_GET['Mn']='12';
+                                                    }else{
+                                                        $calendarDt=date('Y-m-d',strtotime($calendarYr.'-'.($calendarMn-1).'-'.$calendarDy));
+                                                    }
+                                                    header("location:calendar_layout.php?Yr={$_GET['Yr']}&Mn={$_GET['Mn']}");                                                
+                                                }
+
+                                            ?>
+                                    </div>
+
+                                    <div class="form-group col-md-3 px-1">                            
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">Year</span>
+                                            </div>
+                                            <input type="text" class="form-control" name="Yr">
+                                        </div>
+                                    </div> <div class="form-group col-md-3">                            
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">Month</span>
+                                            </div>
+                                            <select name="Mn" class="form-control">
+                                                <option value=""> Choose </option>
+                                                <option value="1"> 1 </option>
+                                                <option value="2"> 2 </option>
+                                                <option value="3"> 3 </option>
+                                                <option value="4"> 4 </option>
+                                                <option value="5"> 5 </option>
+                                                <option value="6"> 6 </option>
+                                                <option value="7"> 7 </option>
+                                                <option value="8"> 8 </option>
+                                                <option value="9"> 9 </option>
+                                                <option value="10"> 10 </option>
+                                                <option value="11"> 11 </option>
+                                                <option value="12"> 12 </option>
+                                            </select>
+                                        </div>
+                                    </div><div class="form-group col-md-1">
+                                        <input type="submit" value="Send" class="btn btn-warning col-12 text-dark" style="font-size: 0.7rem;padding:0.525rem 0.1rem;"></input>
+                                    </div>
+                                    <div class="form-group col-md-2 ml-2 d-none d-md-block">
+                                    <input type="button" class="btn btn-info col-12" style="font-size: 0.7rem;padding:0.525rem 0.1rem;" name="nextMn" value="Next Month">
+                                    </div>
+                                </div> 
+    <?php
+        $calendarDs=date('t',strtotime($calendarDt));//查詢月份的天數
+        $calendarDW=date('l',strtotime($calendarDt));//查詢日期星期幾，預設now
+        $calendarT=date('H:i:s');//查詢時間，預設now
+    if (empty($_GET['Yr']) && empty($_GET['Mn'])){
+        $calendarYr=date('Y',strtotime($calendarDt));//查詢年份，預設now
+        $calendarMn=date('F',strtotime($calendarDt));//查詢月份，預設now  
+    }elseif (isset($_GET['Yr'])){
+        $calendarYr=$_GET['Yr']; //輸入後替換年份
+    }elseif (isset($_GET['Mn'])){
+        $calendarMn=$_GET['Mn']; //輸入後替換月份
+    }else{
+        $calendarYr=$_GET['Yr']; //輸入後替換年份
+        $calendarMn=$_GET['Mn']; //輸入後替換月份
+    }
+    $firstDW=date('w',strtotime($calendarYr.'-'.$calendarMn.'-01'));//第一天星期幾
+    $endDW=date('w',strtotime($calendarYr.'-'.$calendarMn.'-'.$calendarDs));//最後一天星期幾
+?>
+                            </form>
+                        </div>
+
+                        <div class="card-title order-1">
+                            <h1><?=$calendarYrMn;?></h1>
+                        </div>
+
+
+                        <div class="table-responsive order-3" style="border-spacing: 0;">
+                            <table class="table mt-2 ">
+                                <thead class="table-secondary">
+                                    <th class="text-danger">Sun</th>
+                                    <th>Mon</th>
+                                    <th>Tue</th>
+                                    <th>Wed</th>
+                                    <th>Thur</th>
+                                    <th>Fri</th>
+                                    <th class="text-danger">Sat</th>
+                                </thead>
+                                <tbody  class="table bg-white">
+    <?php
+        for ($i=0; $i<=ceil(($calendarDs+$firstDW-7)/7); $i++){
+            echo "<tr>";
+            for ($j=0; $j<=6; $j++){
+                if (($i==0 && $j<$firstDW) ||($i==ceil(($calendarDs+$firstDW-7)/7) && $j>$endDW)) {
+                    echo "<td> &nbsp; </td>";
+                }elseif($j==0||$j==6){
+                    echo "<td class='text-danger font-weight-bold'>";
+                    echo ($i*7)+($j+1)-$firstDW;
+                    echo "</td>";
+                }else{
+                    echo "<td>";
+                    echo ($i*7)+($j+1)-$firstDW;
+                    echo "</td>";
+                }
+            }
+            echo "</tr>";
+        }
+    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
             </div>
-            <div class="col-md-5">
-                <h5 class="text-center text-secondary"><?php echo "Last Month ".$lastMnYrMn ?></h5>
-                <table class="table table-bordered small">
-                    <thead class="bg-secondary text-light transform small">
-                        <th style="padding:0.5rem 0.1rem;">Sun</th>
-                        <th style="padding:0.5rem 0.1rem;">Mon</th>
-                        <th style="padding:0.5rem 0.1rem;">Tue</th>
-                        <th style="padding:0.5rem 0.1rem;">Wed</th>
-                        <th style="padding:0.5rem 0.1rem;">Thur</th>
-                        <th style="padding:0.5rem 0.1rem;">Fri</th>
-                        <th style="padding:0.5rem 0.1rem;">Sat</th>
-                    </thead>
-                    <tbody class="bg-light">
-                    <?php
-                        for ($i=0; $i<=ceil(($lastMnDays+$lastMnFirstDtWk-7)/7); $i++){
-                            echo "<tr>";
-                            for ($j=0; $j<=6; $j++){
-                                if ($i==0 && $j<$lastMnFirstDtWk){
-                                    echo "<td> &nbsp; </td>";
-                                }else if($i==ceil(($lastMnDays+$lastMnFirstDtWk-7)/7) && $j>$lastMnLastDtWk){
-                                    echo "<td> &nbsp; </td>";
-                                }else{
-                                    echo "<td>";
-                                    echo ($i*7)+($j+1)-$lastMnFirstDtWk;
-                                    echo "</td>";
-                                }
-                            }
-                            echo "</tr>";
-                        }
-                    ?>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-md-5">
-                <h5 class="text-center text-secondary"><?php echo "Next Month ".$nextMnYrMn ?></h5>
-                <table class="table table-bordered small">
-                    <thead class="bg-secondary text-light transform small">
-                        <th style="padding:0.5rem 0.1rem;">Sun</th>
-                        <th style="padding:0.5rem 0.1rem;">Mon</th>
-                        <th style="padding:0.5rem 0.1rem;">Tue</th>
-                        <th style="padding:0.5rem 0.1rem;">Wed</th>
-                        <th style="padding:0.5rem 0.1rem;">Thur</th>
-                        <th style="padding:0.5rem 0.1rem;">Fri</th>
-                        <th style="padding:0.5rem 0.1rem;">Sat</th>
-                    </thead>
-                    <tbody class="bg-light">
-                    <?php
-                        for ($i=0; $i<=ceil(($nextMnDays+$nextMnFirstDtWk-7)/7); $i++){
-                            echo "<tr>";
-                            for ($j=0; $j<=6; $j++){
-                                if ($i==0 && $j<$nextMnFirstDtWk){
-                                    echo "<td> &nbsp; </td>";
-                                }else if($i==ceil(($nextMnDays+$nextMnFirstDtWk-7)/7) && $j>$nextMnLastDtWk){
-                                    echo "<td> &nbsp; </td>";
-                                }else{
-                                    echo "<td>";
-                                    echo ($i*7)+($j+1)-$nextMnFirstDtWk;
-                                    echo "</td>";
-                                }
-                            }
-                            echo "</tr>";
-                        }
-                    ?>
-                    </tbody>
-                </table>
-            </div>
+            <div class="mx-1 col-12 col-lg-4 order-1 order-lg-2 card px-2 bg-light text-center align-self-stretch">
+                    <div class="card-body">
+                        <h5 class="text-muted"><?='Today is '.date('Y / m / d')?></h5>
+                        <h1 class="text-warning"><?=$calendarDW?></h1>
+                        <h1 class="text-warning"><?=$calendarT?></h1>
+                        <h4 class="text-muted py-4 d-none d-md-block"><?php echo $timezone;?></h4>
+                        <img src="https://fakeimg.pl/300x200" div class="card-img d-none d-lg-block">
+                    </div>
+            </div>  
         </div>
     </div>
 </body>
