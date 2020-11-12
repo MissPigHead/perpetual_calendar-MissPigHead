718a4cbf9801ffcef68e8219460ccc944e6da885
MissPigHead <misspighead@gmail.com>
2020-11-12 14:08:06 +0800

    using DB for motto data

--- calendar_v2.php
+++ calendar_v2.php
@@ -334,66 +334,17 @@
                         <div class="tab-pane" id="motto" role="tabpanel" aria-labelledby="motto-tab">
                             <blockquote class="blockquote motto-2">
                                 <?php
-                                 $motto = array(
-                                        array('id' => '103','saying' => 'Tell me and I forget. Teach me and I remember. Involve me and I learn.','person' => 'Benjamin Franklin'),
-                                        array('id' => '104','saying' => 'A pessimist is one who makes difficulties of his opportunities and an optimist is one who makes opportunities of his dif','person' => 'Harry S. Truman'),
-                                        array('id' => '105','saying' => 'Always remember that you are absolutely unique. Just like everyone else.','person' => 'Margaret Mead'),
-                                        array('id' => '106','saying' => 'An unexamined life is not worth living.','person' => 'Socrates'),
-                                        array('id' => '107','saying' => 'Challenges are what make life interesting and overcoming them is what makes life meaningful.','person' => 'Joshua J. Marine'),
-                                        array('id' => '108','saying' => 'Don??t judge each day by the harvest you reap but by the seeds that you plant.','person' => 'Robert Louis Stevenson'),
-                                        array('id' => '109','saying' => 'Education is not the filling of a pail but the lighting of a fire.','person' => 'William Butler Yeats'),
-                                        array('id' => '110','saying' => 'Every man is a poet when he is in love.','person' => 'Plato'),
-                                        array('id' => '111','saying' => 'Genius only means hard-working all one??s life.','person' => 'Mendeleyev'),
-                                        array('id' => '112','saying' => 'Happiness lies not in the mere possession of money; it lies in the joy of achievement, in the thrill of creative effort.','person' => 'Franklin Roosevelt'),
-                                        array('id' => '113','saying' => 'I am a slow walker, but I never walk backwards.','person' => 'Abraham Lincoln'),
-                                        array('id' => '114','saying' => 'I can??t change the direction of the wind, but I can adjust my sails to always reach my destination.','person' => 'Jimmy Dean'),
-                                        array('id' => '115','saying' => 'I didn??t fail the test. I just found 100 ways to do it wrong.','person' => 'Benjamin Franklin'),
-                                        array('id' => '116','saying' => 'I disapprove of what you say, but I will defend to the death your right to say it.','person' => 'Voltaire'),
-                                        array('id' => '117','saying' => 'If I looked compared to others far, is because I stand on giant??s shoulder.','person' => 'Newton'),
-                                        array('id' => '118','saying' => 'If you can??t fly, then run; if you can??t run, then walk; if you can??t walk, then crawl; but whatever you do, you have','person' => 'Martin Luther King'),
-                                        array('id' => '119','saying' => 'If you don??t like something, change it; if you can??t change it, change the way you think about it.','person' => 'Mary Engelbreit'),
-                                        array('id' => '120','saying' => 'If you have no critics, you will likely have no success.','person' => 'Malcolm S. Forbes'),
-                                        array('id' => '121','saying' => 'If you judge people, you have no time to love them.','person' => 'Mother Teresa'),
-                                        array('id' => '122','saying' => 'If you look at what you have in life, you??ll always have more. If you look at what you don??t have in life, you??ll nev','person' => 'Oprah Winfrey'),
-                                        array('id' => '123','saying' => 'Imagination is more important than knowledge.','person' => 'Albert Einstein'),
-                                        array('id' => '124','saying' => 'Intelligence plus character ?V that is the goal of real education.','person' => 'Martin Luther King'),
-                                        array('id' => '125','saying' => 'It is not how much you do, but how much love you put into the doing that matters.','person' => 'Mother Teresa'),
-                                        array('id' => '126','saying' => 'Knowledge is a treasure, but practice is the key to it.','person' => 'British churchman ?Thomas Full'),
-                                        array('id' => '127','saying' => 'Knowledge is power.','person' => 'Francis Bacon'),
-                                        array('id' => '128','saying' => 'Life is like riding a bicycle. To keep your balance you must keep moving.','person' => 'Albert Einstein'),
-                                        array('id' => '129','saying' => 'Living without an aim is like sailing without a compass.','person' => 'Alexandre Dumas'),
-                                        array('id' => '130','saying' => 'Love the life you live. Live the life you love.','person' => 'Bob Marley'),
-                                        array('id' => '131','saying' => 'Love well, whip well.','person' => 'Benjamin Franklin'),
-                                        array('id' => '132','saying' => 'Never argue with stupid people, they will drag you down to their level and then beat you with experience.','person' => 'Mark Twain'),
-                                        array('id' => '133','saying' => 'Success is not final, failure is not fatal: it is the courage to continue that counts.','person' => 'Winston Churchill'),
-                                        array('id' => '134','saying' => 'The best and most beautiful things in the world cannot be seen or even touched. They must be felt with the heart.','person' => 'Helen Keller'),
-                                        array('id' => '135','saying' => 'The first wealth is health.','person' => 'Ralph Waldo Emerson'),
-                                        array('id' => '136','saying' => 'The greatest glory in living lies not in never falling, but in rising every time we fall.','person' => 'Nelson Mandela'),
-                                        array('id' => '137','saying' => 'The only thing we have to fear is fear itself.','person' => 'Franklin Roosevelt'),
-                                        array('id' => '138','saying' => 'The only way to do great work is to love what you do. If you haven??t found it yet, keep looking. Don??t settle.','person' => 'Steve Jobs'),
-                                        array('id' => '139','saying' => 'The ordinary focus on what they\'re getting. The extraordinary think about who they\'re becoming.','person' => 'Robin Sharma'),
-                                        array('id' => '140','saying' => 'The past cannot be changed. The future is yet in your power.','person' => 'Mary Pickford'),
-                                        array('id' => '141','saying' => 'The purpose of our lives is to be happy.','person' => 'Dalai Lama'),
-                                        array('id' => '142','saying' => 'The way to get started is to quit talking and begin doing.','person' => 'Walt Disney'),
-                                        array('id' => '143','saying' => 'There are seven things that will destroy us: wealth without work; pleasure without conscience; knowledge without charact','person' => 'Mahatma Gandhi'),
-                                        array('id' => '144','saying' => 'Those who dare to fail miserably can achieve greatly.','person' => 'John F. Kennedy'),
-                                        array('id' => '145','saying' => 'Try not to become a man of success but rather try to become a man of value.','person' => 'Albert Einstein'),
-                                        array('id' => '146','saying' => 'Until we can manage time, we can manage nothing else.','person' => 'Peter F. Drucker'),
-                                        array('id' => '147','saying' => 'We must accept finite disappointment, but we must never lose infinite hope.','person' => 'Martin Luther King'),
-                                        array('id' => '148','saying' => 'When the whole world is silent, even one voice becomes powerful.','person' => 'Malala'),
-                                        array('id' => '149','saying' => 'When we are saying this cannot be accomplished, this cannot be done, then we are short-changing ourselves. My brain, it ','person' => 'Kobe Bryant'),
-                                        array('id' => '150','saying' => 'Where there is a will, there is a way.','person' => 'Thomas Edison'),
-                                        array('id' => '151','saying' => 'You have to believe in yourself . That??s the secret of success.','person' => 'Charles Chaplin'),
-                                        array('id' => '152','saying' => 'You must be the change you want to see in the world.','person' => 'Gandhi')
-                                      );
-                                        $randMotto=rand(0,49);
-                                        $saying=$motto[$randMotto]['saying'];
-                                        $person=$motto[$randMotto]['person'];
-                                    
-                                    
+/* using DB here*/                  $dsn="mysql:host=localhost;dbname=motto;charset=utf8";
+                                    $pdo= new PDO($dsn,'root','');
+                                    $mottos=$pdo -> query("select * from motto") -> fetchAll(PDO::FETCH_NUM);
+                                    foreach ($mottos as $motto){
+                                        $saying[]=$motto['1'];
+                                        $person[]=$motto['2'];
+                                    }
+                                    $randMotto=rand(0,49);
                                 ?>
-                                <p><?=$saying;?></p>
-                                <footer class="blockquote-footer motto-3 text-right"><cite title="Source Title"><?=$person;?></cite></footer>
+                                <p><?=$saying[$randMotto];?></p>
+                                <footer class="blockquote-footer motto-3 text-right"><cite title="Source Title"><?=$person[$randMotto];?></cite></footer>
                             </blockquote>
                         </div>
                     </div>
